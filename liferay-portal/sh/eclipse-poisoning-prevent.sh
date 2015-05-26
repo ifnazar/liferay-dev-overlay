b4d2f9d39de38488944cdeab7b61ec821b76a802
André de Oliveira <andre.oliveira@liferay.com>
2015-05-26 17:25:54 -0300

    Billion laughs

---
+++ liferay-portal/sh/eclipse-poisoning-prevent.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -o errexit ; set -o nounset
+
+cd ${LIFERAY_PORTAL_DIR}
+
+if [ -e "portal-impl/test-classes/unit/com/liferay/portal/security/xml/dependencies/xml-bomb-billion-laughs.xml" ]
+	then rm portal-impl/test-classes/unit/com/liferay/portal/security/xml/dependencies/xml-bomb-*.xml
+fi
+
+if [ -e "portal-impl/test/unit/com/liferay/portal/security/xml/dependencies/xml-bomb-billion-laughs.xml" ]
+	then rm portal-impl/test/unit/com/liferay/portal/security/xml/dependencies/xml-bomb-*.xml
+fi
--- liferay-portal/sh/unchange.sh
+++ liferay-portal/sh/unchange.sh
@@ -6,16 +6,26 @@ cd ${LIFERAY_PORTAL_DIR}
 
 git update-index --assume-unchanged .classpath
 git update-index --assume-unchanged .ivy/cache/--default.xml
+git update-index --assume-unchanged portal-impl/test/unit/com/liferay/portal/security/xml/dependencies/xml-bomb-billion-laughs.xml
+git update-index --assume-unchanged portal-impl/test/unit/com/liferay/portal/security/xml/dependencies/xml-bomb-quadratic-blowup.xml
+git update-index --assume-unchanged modules/apps/bookmarks/bookmarks-api/.classpath
 git update-index --assume-unchanged modules/apps/bookmarks/bookmarks-test/.classpath
+git update-index --assume-unchanged modules/apps/bookmarks/bookmarks-test/ivy.xml
+git update-index --assume-unchanged modules/apps/dynamic-data-lists/dynamic-data-lists-test/.classpath
 git update-index --assume-unchanged modules/apps/wiki/wiki-service/.classpath
 git update-index --assume-unchanged modules/apps/wiki/wiki-test/.classpath
 git update-index --assume-unchanged modules/apps/wiki/wiki-web/.classpath
 git update-index --assume-unchanged modules/core/portal-bootstrap/.classpath
 git update-index --assume-unchanged modules/core/registry-impl/.classpath
 git update-index --assume-unchanged modules/portal/portal-search-elasticsearch/.classpath
+git update-index --assume-unchanged modules/util/source-formatter/.classpath
 
 cd ${LIFERAY_PLUGINS_DIR}
 
+if [ -e "apps/content-targeting" ] 
+then
+	git update-index --assume-unchanged apps/content-targeting
+fi
 git update-index --assume-unchanged shared/solr4-shared/.classpath
 git update-index --assume-unchanged shared/solr4-shared/lib/portal-compat-shared.jar
 git update-index --assume-unchanged webs/solr4-web/.classpath
