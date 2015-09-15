b89d2d9566b23405d51297f1f97d1420f6f2f3a2
André de Oliveira <andre.oliveira@liferay.com>
2015-09-15 16:18:40 -0300

    I can has Gradle

--- liferay-portal/sh/ant-setup-eclipse.sh
+++ liferay-portal/sh/ant-setup-eclipse.sh
@@ -8,18 +8,27 @@ function ant_setup_eclipse()
 {
 	subdir=$1
 	cd ${LIFERAY_PORTAL_DIR}/$subdir
-	ant setup-eclipse
+	${LIFERAY_PORTAL_DIR}/gradlew eclipse
 }
 
 
 
-ant_setup_eclipse modules/apps/bookmarks/bookmarks-test
-ant_setup_eclipse modules/apps/document-library/document-library-service
-ant_setup_eclipse modules/apps/document-library/document-library-test
-ant_setup_eclipse modules/apps/dynamic-data-lists/dynamic-data-lists-test
-ant_setup_eclipse modules/apps/item-selector/item-selector-api
-ant_setup_eclipse modules/apps/item-selector/item-selector-criteria-api
-ant_setup_eclipse modules/apps/journal/journal-test
-ant_setup_eclipse modules/apps/message-boards/message-boards-test
-ant_setup_eclipse modules/apps/wiki/wiki-test
+ant_setup_eclipse modules/apps/application-list
+ant_setup_eclipse modules/apps/bookmarks
+ant_setup_eclipse modules/apps/document-library
+ant_setup_eclipse modules/apps/dynamic-data-lists
+ant_setup_eclipse modules/apps/dynamic-data-mapping
+ant_setup_eclipse modules/apps/export-import
+ant_setup_eclipse modules/apps/item-selector
+ant_setup_eclipse modules/apps/journal
+ant_setup_eclipse modules/apps/message-boards
+ant_setup_eclipse modules/apps/mobile-device-rules
+ant_setup_eclipse modules/apps/portal-settings
+ant_setup_eclipse modules/apps/wiki
+ant_setup_eclipse modules/core/osgi-service-tracker-map
 ant_setup_eclipse modules/portal/portal-rest-extender-test
+ant_setup_eclipse modules/portal/portal-search
+ant_setup_eclipse modules/portal/portal-search-elasticsearch
+ant_setup_eclipse modules/portal/portal-search-solr
+ant_setup_eclipse modules/portal/portal-settings
+ant_setup_eclipse modules/test/arquillian-extension-junit-bridge
