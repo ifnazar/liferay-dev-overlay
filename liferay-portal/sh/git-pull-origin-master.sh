08b948527110b7cebe92655c8369c98b17ce67fb
André de Oliveira <andre.oliveira@liferay.com>
2016-05-17 15:53:36 -0300

    Pull rebase from custom remote (e.g. local auto pull instead of origin)

--- liferay-portal/sh/git-pull-origin-master.sh
+++ liferay-portal/sh/git-pull-origin-master.sh
@@ -4,5 +4,16 @@ set -o errexit ; set -o nounset
 
 cd ${LIFERAY_PORTAL_DIR}
 
-git remote --verbose show origin
-git pull origin master
+figlet pull rebase || true
+
+#REMOTE_NAME=origin
+REMOTE_NAME=1-portal-local-auto-pull
+BRANCH_NAME=master
+
+echo =======================================================================
+git remote show ${REMOTE_NAME}
+echo =======================================================================
+git status
+echo =======================================================================
+git pull --rebase ${REMOTE_NAME} ${BRANCH_NAME}
+echo =======================================================================
