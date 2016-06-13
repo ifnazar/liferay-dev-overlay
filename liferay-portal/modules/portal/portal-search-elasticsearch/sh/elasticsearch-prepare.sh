a9947cc028beeb9ceef6d3e98b082368d991d7d5
rodrigopaulino <rodrigo.paulino@liferay.com>
2016-06-13 09:23:53 -0300

    Updating elasticsearch prepare script

--- liferay-portal/modules/portal/portal-search-elasticsearch/sh/elasticsearch-prepare.sh
+++ liferay-portal/modules/portal/portal-search-elasticsearch/sh/elasticsearch-prepare.sh
@@ -2,12 +2,19 @@
 
 set -o errexit ; set -o nounset
 
-RESOURCES_DIR=${LIFERAY_PORTAL_DIR}/modules/apps/portal-search/portal-search-elasticsearch/src/main/resources
-ELASTICSEARCH_DIR=${LIFERAY_BUNDLES_PORTAL_DIR}/elasticsearch-server/elasticsearch-2.2.0/
+RESOURCES_DIR=${LIFERAY_PORTAL_DIR}/modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/resources
+ELASTICSEARCH_DIR=${LIFERAY_BUNDLES_PORTAL_DIR}/elasticsearch-server/elasticsearch-2.2.0
 DATA_DIR=${ELASTICSEARCH_DIR}/data
 LIFERAYELASTICSEARCHCLUSTER_DIR=${DATA_DIR}/LiferayElasticsearchCluster
 
 # Replacements
-#cp   ${RESOURCES_DIR}/config/elasticsearch.yml       ${INSTANCE_DIR}/conf
+cp   ${RESOURCES_DIR}/config/elasticsearch.yml       ${ELASTICSEARCH_DIR}/config
 
-rm -r   ${LIFERAYELASTICSEARCHCLUSTER_DIR}
+rm -r   ${LIFERAYELASTICSEARCHCLUSTER_DIR}    ||  true
+
+cd ${ELASTICSEARCH_DIR}
+
+bin/plugin install analysis-kuromoji || true
+bin/plugin install analysis-icu || true
+bin/plugin install analysis-smartcn || true
+bin/plugin install analysis-stempel || true
