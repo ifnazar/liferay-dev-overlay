4645b694d9f560b71b00070fddc8573e429992d4
André de Oliveira <andre.oliveira@liferay.com>
2017-05-31 19:43:35 -0700

    Elasticsearch 2.4

--- liferay-portal/modules/portal/portal-search-elasticsearch/sh/elasticsearch-prepare.sh
+++ liferay-portal/modules/portal/portal-search-elasticsearch/sh/elasticsearch-prepare.sh
@@ -2,13 +2,13 @@
 
 set -o errexit ; set -o nounset
 
-RESOURCES_DIR=${LIFERAY_PORTAL_DIR}/modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/resources
-ELASTICSEARCH_DIR=${LIFERAY_BUNDLES_PORTAL_DIR}/elasticsearch-server/elasticsearch-2.2.0
+RESOURCES_DIR=${LIFERAY_PORTAL_DIR}/modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/resources/META-INF/resources
+ELASTICSEARCH_DIR=${LIFERAY_BUNDLES_PORTAL_DIR}/elasticsearch-server/elasticsearch-2.4.0
 DATA_DIR=${ELASTICSEARCH_DIR}/data
 LIFERAYELASTICSEARCHCLUSTER_DIR=${DATA_DIR}/LiferayElasticsearchCluster
 
 # Replacements
-cp   ${RESOURCES_DIR}/config/elasticsearch.yml       ${ELASTICSEARCH_DIR}/config
+#cp   ${RESOURCES_DIR}/elasticsearch.yml       ${ELASTICSEARCH_DIR}/config
 
 rm -r   ${LIFERAYELASTICSEARCHCLUSTER_DIR}    ||  true
 
--- liferay-portal/modules/portal/portal-search-elasticsearch/sh/elasticsearch-start.sh
+++ liferay-portal/modules/portal/portal-search-elasticsearch/sh/elasticsearch-start.sh
@@ -2,8 +2,9 @@
 
 set -o errexit ; set -o nounset
 
-ELASTICSEARCH_DIR=${LIFERAY_BUNDLES_PORTAL_DIR}/elasticsearch-server/elasticsearch-2.2.0/
+ELASTICSEARCH_DIR=${LIFERAY_BUNDLES_PORTAL_DIR}/elasticsearch-server/elasticsearch-2.4.0
 
 cd ${ELASTICSEARCH_DIR}
 
 bin/elasticsearch
+#  -Des.logger.level=DEBUG
