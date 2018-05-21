cd1c53edbe5926de5587a137c1b707bc8d2943b8
André de Oliveira <andre.oliveira@liferay.com>
2018-05-21 11:09:11 -0700

    package.json and package-lock.json regenerate locally at random times

--- liferay-portal/sh/unchange.sh
+++ liferay-portal/sh/unchange.sh
@@ -9,12 +9,14 @@ figlet Unchange || true
 cd ${LIFERAY_PORTAL_DIR}
 
 git update-index --assume-unchanged .classpath
+git update-index --assume-unchanged modules/apps/frontend-theme/frontend-theme-classic/package.json
 git update-index --assume-unchanged portal-impl/test/unit/com/liferay/portal/security/xml/dependencies/xml-bomb-billion-laughs.xml
 git update-index --assume-unchanged portal-impl/test/unit/com/liferay/portal/security/xml/dependencies/xml-bomb-quadratic-blowup.xml
 git update-index --assume-unchanged portal-test-integration/src/com/liferay/portal/test/rule/LogAssertionHandler.java
 
 git ls-files --deleted | grep settings.gradle | xargs git update-index --assume-unchanged
 git ls-files --deleted | grep .lfrbuild-portal | xargs git update-index --assume-unchanged
+git ls-files --modified | grep package-lock.json | xargs git update-index --assume-unchanged
 
 cd ${LIFERAY_PLUGINS_DIR}
 
