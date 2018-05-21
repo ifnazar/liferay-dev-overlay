ea9a3973db749e4cb345c9457a5e9667c12bc19e
André de Oliveira <andre.oliveira@liferay.com>
2018-05-21 11:00:10 -0700

    portal-ext.properties can live at liferay-portal above tomcat, safe from ant nightly

--- liferay-bundles/sh/fn-portal-debug.sh
+++ liferay-bundles/sh/fn-portal-debug.sh
@@ -12,7 +12,8 @@ function tomcat_debug()
 	LPORTAL_DATABASE=$1
 	TOMCAT_DIR=$2
 
-	property_set    jdbc.default.url     jdbc:mysql://localhost/${LPORTAL_DATABASE}?useUnicode=true\\\&characterEncoding=UTF-8\\\&useFastDateParsing=false     ${TOMCAT_DIR}/webapps/ROOT/WEB-INF/classes/portal-ext.properties
+	property_set    jdbc.default.url     jdbc:mysql://localhost/${LPORTAL_DATABASE}?useUnicode=true\\\&characterEncoding=UTF-8\\\&useFastDateParsing=false     ${TOMCAT_DIR}/../portal-ext.properties
+	# ${TOMCAT_DIR}/webapps/ROOT/WEB-INF/classes/portal-ext.properties
 
 	${TOMCAT_DIR}/bin/catalina.sh jpda run
 }
